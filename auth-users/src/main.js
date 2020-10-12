5ec8d9b30638c12c723f7ee0713f34920a8b9fbe
DouglasPapu <douglas.lopez1@correo.icesi.edu.co>
2020-10-11 22:31:13 -0500

    vuex and vue-router installed. Its required

--- auth-users/src/main.js
+++ auth-users/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from "./plugins/vuetify";
+import VueRouter from "vue-router";
+import store from "./store";
+import HelloWorld from "./components/HelloWorld"
+const routes = [
+  {
+    path: "/",
+    component: HelloWorld,
+  },
+];
+
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({ routes });
+
+Vue.use(VueRouter);
+
 new Vue({
+  vuetify,
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
