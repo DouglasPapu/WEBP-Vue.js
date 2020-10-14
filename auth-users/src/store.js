26b96d83dc88993123a67fc59a92c5b0da133ca9
DouglasPapu <douglas.lopez1@correo.icesi.edu.co>
2020-10-14 11:55:27 -0500

    Dependency arrays users and dependecies done!

--- auth-users/src/store.js
+++ auth-users/src/store.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import {db} from "./main"
+import firebase from "firebase/app";
 
 Vue.use(Vuex);
 
@@ -12,15 +13,26 @@ export default new Vuex.Store({
     mutations: {
         addToUsers(state,payload){
             try{
-                payload.id=((state.users.length) - 1) + 1;
+                payload.id=((state.users.length) - 1) + 1; //cambiarlo
                 state.users.push(payload);
                 db.collection('users').add(payload);
 
+                //Agrega el usuario a la lista de usuarios de la dependencia correspondiente. En el store.
+                state.dependencies.forEach(dep => {
+                    if(dep.id === payload.dependency){
+                        dep.users.push(payload);
+                    }
+                });
+                //Agrega el usuario a la lista de usuarios de la dependencia correspondiente. En firebase.
                 db.collection("dependencies").get().then((dep) => {
                     dep.forEach(doc => {
-                      
+                        if(doc.data().id === payload.dependency){
+                            db.collection("dependencies").doc(doc.id).update({
+                                users: firebase.firestore.FieldValue.arrayUnion(payload)
+                            });
+                        }
                     })
-                 });
+                 });   
 
             }catch(error){
                 console.log(error);
