4a54d4638fbc87b845c5eecfd84c7669d94a1029
DouglasPapu <douglas.lopez1@correo.icesi.edu.co>
2020-10-13 23:06:55 -0500

    Users and Dependencies tables done! Search is coming!

--- auth-users/src/components/Dependencies.vue
+++ auth-users/src/components/Dependencies.vue
@@ -1,5 +1,14 @@
 <template>
   <v-card width="1000" shaped elevation="17" class="mx-auto mt-9">
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
     <v-simple-table>
       <template v-slot:default>
         <thead>
@@ -17,11 +26,36 @@
             <td>{{ item.name }}</td>
             <td>{{ item.coordinator }}</td>
             <td>{{ item.location }}</td>
-            <td>{{ item.active }}</td>
+            <td>
+              <v-icon :color="item.active ? 'green darken-2' : 'red'">{{item.active ? "verified" : "unpublished"}}</v-icon>
+            </td>
             <td>{{ item.amountUsers }}</td>
-            <td><v-icon>edit</v-icon> <v-icon>delete</v-icon></td>
+            <td><v-icon small @click="viewDependency(item)">visibility</v-icon> <v-icon small @click="editDependency(item)">edit</v-icon> <v-icon small @click="dialogDelete = true">delete</v-icon></td>
           </tr>
         </tbody>
+        <!-- Dialog to show dependency details-->
+            <v-dialog v-model="dialogDetails" max-width="500px">
+             <v-card>
+              <v-card-title class="headline">Details of {{itemView.name}}</v-card-title>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                   <v-btn color="blue darken-1" text @click="viewItemConfirm">OK</v-btn>
+                 <v-spacer></v-spacer>
+               </v-card-actions>
+              </v-card>
+            </v-dialog>
+             <!-- Dialog to show warning -->
+             <v-dialog v-model="dialogDelete" max-width="500px">
+               <v-card>
+               <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+                 <v-card-actions>
+                  <v-spacer></v-spacer>
+                   <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                   <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                   <v-spacer></v-spacer>
+                 </v-card-actions>
+               </v-card>
+             </v-dialog>
       </template>
     </v-simple-table>
   </v-card>
@@ -31,8 +65,26 @@
 export default {
   data() {
     return {
-
+      itemView: Object,
+      search: "",
+      dialogDelete: false,
+      dialogDetails: false,
     };
+  },
+  methods: {
+      closeDelete(){
+         this.dialogDelete = false;
+      },
+      deleteItemConfirm(){
+        this.dialogDelete = false;
+      },
+      viewDependency(item){
+        this.itemView = item;
+        this.dialogDetails = true;
+      },
+      viewItemConfirm(){
+        this.dialogDetails = false;
+      },
   },
    computed:{
       dependencies(){
--- auth-users/src/components/Users.vue
+++ auth-users/src/components/Users.vue
@@ -1,5 +1,14 @@
 <template>
   <v-card width="1300" shaped elevation="17" class="mx-auto mt-9">
+     <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
     <v-simple-table>
       <template v-slot:default>
         <thead>
@@ -22,12 +31,35 @@
             <td>{{ item.initDate }}</td>
             <td>{{ item.finalDate }}</td>
             <td>{{ item.dependency }}</td>
-            <td>{{ item.active }}</td>
+            <td><v-icon :color="item.active ? 'green darken-2' : 'red'">{{item.active ? "how_to_reg" : "unpublished"}}</v-icon></td>
             <td>
-              <v-icon>edit</v-icon> <v-icon>delete</v-icon>
+              <v-icon small @click="viewUser(item)">visibility</v-icon> <v-icon small @click="editUser(item)">edit</v-icon> <v-icon small @click="dialogDelete = true">delete</v-icon>
             </td>
           </tr>
         </tbody>
+        <!-- Dialog to show user details-->
+            <v-dialog v-model="dialogDetails" max-width="500px">
+             <v-card>
+              <v-card-title class="headline">Details of {{itemView.name}}</v-card-title>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                   <v-btn color="blue darken-1" text @click="viewItemConfirm">OK</v-btn>
+                 <v-spacer></v-spacer>
+               </v-card-actions>
+              </v-card>
+            </v-dialog>
+             <!-- Dialog to show warning -->
+             <v-dialog v-model="dialogDelete" max-width="500px">
+               <v-card>
+               <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+                 <v-card-actions>
+                  <v-spacer></v-spacer>
+                   <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                   <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                   <v-spacer></v-spacer>
+                 </v-card-actions>
+               </v-card>
+             </v-dialog>
       </template>
     </v-simple-table>
   </v-card>
@@ -37,8 +69,26 @@
 export default {
   data() {
     return {
-
+      itemView: Object,
+      search: "",
+      dialogDelete: false,
+      dialogDetails: false,
     }
+  },
+  methods: {
+      closeDelete(){
+         this.dialogDelete = false;
+      },
+      deleteItemConfirm(){
+        this.dialogDelete = false;
+      },
+      viewUser(item){
+        this.itemView = item;
+        this.dialogDetails = true;
+      },
+      viewItemConfirm(){
+        this.dialogDetails = false;
+      },
   },
    computed:{
       users(){
